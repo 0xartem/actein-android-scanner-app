11a1fefc65a7cf4f17db23ad524a82d6e2632d06
Artem Brazhnikov <artem.bv@protonmail.com>
2017-07-26 14:45:59 +0200

    set up timer to null after it stopped

--- scanner/src/main/java/com/actein/helpers/VrStationTimer.java
+++ scanner/src/main/java/com/actein/helpers/VrStationTimer.java
@@ -43,6 +43,7 @@ public class VrStationTimer
                 {
                     mGameCountDownTimer.cancel();
                     mGameCountDownTimer.onFinish();
+                    mGameCountDownTimer = null;
                 }
             });
         }
